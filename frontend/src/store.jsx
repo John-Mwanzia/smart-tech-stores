2b22549f0606d213b3ea7b9a0c4ff0adfc9901ca
John mwanzia <johnmwanzia277@gmail.com>
2023-02-04 22:40:13 -0800

    ipmlement localstorage

--- frontend/src/store.jsx
+++ frontend/src/store.jsx
@@ -13,10 +13,12 @@ const reducer = (state, action)=>{
             const existItem = state.cart.cartItems.find((item) =>item._id===newItem._id)
             const cartItems = existItem? state.cart.cartItems.map((item) =>item._id === existItem._id? newItem : item
             ) :[...state.cart.cartItems, newItem];
+            localStorage.setItem("cartItems", JSON.stringify(cartItems))
             return {...state, cart: {...state.cart, cartItems}}
 
         case 'REMOVE-CART-ITEM': {
                 const cartItems = state.cart.cartItems.filter( (item) => item._id !== action.payload._id);
+                localStorage.setItem("cartItems", JSON.stringify(cartItems))
                 return  {...state, cart: {...state.cart, cartItems}}
              }
         default:
@@ -27,7 +29,9 @@ const reducer = (state, action)=>{
 export default function StoreProvider(props) {
     const [state, dispatch] = useReducer(reducer, {
        cart: {
-        cartItems: []
+        cartItems: localStorage.getItem("cartItems")?  
+        JSON.parse(localStorage.getItem("cartItems")) 
+         : []
        } 
     })
    
