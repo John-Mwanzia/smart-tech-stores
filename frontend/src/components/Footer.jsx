b03530a77c36cd9f8952820601d3bd2c59f52eac
John mwanzia <johnmwanzia277@gmail.com>
2023-04-22 12:28:18 +0300

    impemented footer section

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,36 +3,65 @@ import React from "react";
 export default function Footer() {
   return (
     <div>
-      <footer className="mt-4 bg-slate-800">
-        <div className=" footer-wrapper">
-          <div>
-            <h2>About</h2>
-            {/* <p>
-              Welcome to smart-tech, your one-stop-shop for all your technology
-              needs. Our mission is to provide our customers with the latest and
-              greatest technology at affordable prices, while also delivering
-              exceptional customer service and support. Our company was founded
-              in 2010 by a team of technology enthusiasts who wanted to create a
-              better way for people to buy and enjoy the latest gadgets and
-              electronics. Since then, we've grown into a leading eCommerce
-              store, offering a wide range of products from the world's top
-              technology brands. At smart-tech, we're committed to
-              sustainability and social responsibility. We work with our
-              suppliers and partners to ensure that our products are ethically
-              sourced and manufactured. We believe in putting our customers
-              first, and we're dedicated to making your shopping experience with
-              us as easy and enjoyable as possible. If you have any questions or
-              feedback, please don't hesitate to contact us. We're always happy
-              to hear from our customers and are committed to providing the best
-              possible service. Thank you for choosing Smart-tech for all your
-              technology needs!"
+      <footer className="mt-4 bg-slate-800 ">
+        <div className=" footer-wrapper flex flex-row ">
+          <div className="about-wapper">
+            <h2  className="text-center">About us</h2>
+            {/* <p className="max-w-max">
+              Welcome to our web app! We are passionate about providing
+              high-quality computer accessories and electronics to our
+              customers. Our team consists of tech enthusiasts who understand
+              the importance of having reliable and efficient devices that meet
+              the demands of modern life. We are committed to providing a
+              seamless and enjoyable shopping experience for our customers. From
+              finding the perfect accessory to navigating through the checkout
+              process, we are here to make sure your shopping journey is easy
+              and hassle-free. Our product selection includes a wide range of
+              computer accessories and electronics, from cables and chargers to
+              headphones and speakers. We carefully curate our inventory to
+              ensure that each product meets our high standards for quality and
+              performance. At our web app, we value our customers above all
+              else. We believe in building long-lasting relationships based on
+              trust and mutual respect. If you have any questions or concerns,
+              please don't hesitate to contact us. We are always here to help.
+              Thank you for choosing our web app for your computer accessory and
+              electronics needs. We look forward to serving you!
             </p> */}
+            <div>
+              <p>About smart-tech</p>
+              <p>Terms and conditions</p>
+              <p>privacy policy</p>
+            </div>
           </div>
-          <div>
-            <h2>Contact Us</h2>
+          <div  className="text-center">
+            <h2 className="text-center">Contact Us</h2>
+            <div>
+              <p>Phone: 123-456-7890</p>
+              <p>
+                Email :{" "}
+                <a
+                  href="mailto:
+              smarttech29@gmail.com"
+                >
+                  smarttech29@gmail.com
+                </a>
+              </p>
+              <p>Chat support</p>
+            </div>
           </div>
-          <div>
-            <h2>Follow us</h2>
+          <div  className="text-center">
+            <h2  className="text-center">Follow us</h2>
+            <div className="icon-wrapper">
+              <div>
+                <img src="/images/facebook.png" width={40} height={40} />
+              </div>
+              <div>
+                <img src="/images/instagram.png" width={40} height={40} />
+              </div>
+              <div>
+                <img src="/images/twitter.png" width={40} height={40} />
+              </div>
+            </div>
           </div>
         </div>
       </footer>
