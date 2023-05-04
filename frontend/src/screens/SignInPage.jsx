49cc0159124186da880e7d379a3712d1b5bc2510
John mwanzia <johnmwanzia277@gmail.com>
2023-05-04 15:01:33 +0300

    implemented submithandler for the signin page form

--- frontend/src/screens/SignInPage.jsx
+++ frontend/src/screens/SignInPage.jsx
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function SignInPage() {
+  const {email, setEmail} = useState("")
+  const {password, setPassword}= useState();
+
+  const submitHandler = (e)=>{
+    e.preventDefault()
+  }
   return (
     <> 
         <div className="signin-wrapper">
          <div class="container-signin  bg-black opacity-60 ">
         <h2 className="text-center">SignIn</h2>
-        <form action="">
+        <form onSubmit={submitHandler}>
             <div class="user-box">
-                <input type="Email" name="" required/>
+                <input type="Email" name="" required onChange={ e => setEmail(e.target.value)} />
                 <label >Email</label>
             </div>
             <div class="user-box">
-                <input type="password" name="" required/>
+                <input type="password" name="" required  onChange={ e => setPassword(e.target.value)} />
                 <label for="">Password</label>
             </div>
             <a href="#">
