a9e12232c88b2d4d965316b92139bae8c6aba071
John mwanzia <johnmwanzia277@gmail.com>
2023-05-04 16:31:49 +0300

    implemented signin authentication

--- frontend/src/screens/SignInPage.jsx
+++ frontend/src/screens/SignInPage.jsx
@@ -1,33 +1,47 @@
+import axios from "axios";
 import React, { useState } from "react";
 
 export default function SignInPage() {
-  const {email, setEmail} = useState("")
-  const {password, setPassword}= useState();
+  const [email, setEmail] = useState("")
+  const [password, setPassword]= useState();
 
-  const submitHandler = (e)=>{
+  const submitHandler = async(e)=>{
     e.preventDefault()
+    try { 
+      //send  a post  request with email & passsword variables and deconstruct data from the response
+      const {data} = await axios.post("http://localhost:3000/api/users/signin", {
+        email,
+        password
+      });
+      console.log(data);
+    } catch (err) {
+      
+    }
   }
   return (
     <> 
         <div className="signin-wrapper">
-         <div class="container-signin  bg-black opacity-60 ">
+         <div className="container-signin  bg-black opacity-60 ">
         <h2 className="text-center">SignIn</h2>
         <form onSubmit={submitHandler}>
-            <div class="user-box">
-                <input type="Email" name="" required onChange={ e => setEmail(e.target.value)} />
+            <div className="user-box">
+                <input type="Email" name="" required onChange={ (e) => setEmail(e.target.value)} />
                 <label >Email</label>
             </div>
-            <div class="user-box">
-                <input type="password" name="" required  onChange={ e => setPassword(e.target.value)} />
-                <label for="">Password</label>
+            <div className="user-box">
+                <input type="password" name="" required  onChange={ (e) => setPassword(e.target.value)} />
+                <label>Password</label>
             </div>
-            <a href="#">
+            <div className="button-wrapper" href="#">
                 <span></span>
                 <span></span>
                 <span></span>
                 <span></span>
-                Submit
-               </a> 
+                <button type="submit">
+                  Submit
+                </button>
+               
+               </div> 
         </form>
     </div>
     </div>
