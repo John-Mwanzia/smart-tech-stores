45b81bd87eb5f167911cc1a48a965fd40d637ef8
John mwanzia <johnmwanzia277@gmail.com>
2023-05-08 15:57:56 +0300

    created google login button

--- frontend/src/screens/SignInPage.jsx
+++ frontend/src/screens/SignInPage.jsx
@@ -2,67 +2,86 @@ import axios from "axios";
 import React, { useContext, useState } from "react";
 import { Store } from "../store";
 import { Link, useLocation, useNavigate } from "react-router-dom";
+import GoogleLogin from "react-google-login";
 
 export default function SignInPage() {
   const [email, setEmail] = useState("");
-  const [password, setPassword]= useState("");
- 
-  const navigate = useNavigate()
- //get redirect value from URL
-  const {search} = useLocation();
-  const redirectInUrl = new URLSearchParams(search).get("redirect")
-  const redirect = redirectInUrl? redirectInUrl: "/"
-  const {state, dispatch: ctxDispatch} = useContext(Store)
+  const [password, setPassword] = useState("");
 
-  const submitHandler = async(e) => {
+  const navigate = useNavigate();
+  //get redirect value from URL
+  const { search } = useLocation();
+  const redirectInUrl = new URLSearchParams(search).get("redirect");
+  const redirect = redirectInUrl ? redirectInUrl : "/";
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+
+  const submitHandler = async (e) => {
     e.preventDefault();
-    try { 
+    try {
       //send  a post  request with email & passsword variables and deconstruct data from the response
-      const { data } = await axios.post("http://localhost:3000/api/users/signin", {
-        email,
-        password,
-      });
-      ctxDispatch({type : 'USER_SIGNIN', payload: data});
+      const { data } = await axios.post(
+        "http://localhost:3000/api/users/signin",
+        {
+          email,
+          password,
+        }
+      );
+      ctxDispatch({ type: "USER_SIGNIN", payload: data });
       localStorage.setItem("userInfo", JSON.stringify(data));
-      navigate(redirect || "/") 
-     
-     
+      navigate(redirect || "/");
     } catch (error) {
       alert("invalid email or password");
-    } 
+    }
   };
   return (
-    <> 
-        <div className="signin-wrapper">
-         <div className="container-signin  bg-black opacity-60 ">
-        <h2 className="text-center">SignIn</h2>
-        <form onSubmit={submitHandler}>
+    <>
+      <div className="signin-wrapper">
+        <div className="container-signin  bg-black opacity-60 ">
+          <h2 className="text-center">SignIn</h2>
+          <form onSubmit={submitHandler}>
             <div className="user-box">
-                <input type="Email" name="" required onChange={ (e) => setEmail(e.target.value)} />
-                <label >Email</label>
+              <input
+                type="Email"
+                name=""
+                required
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <label>Email</label>
             </div>
             <div className="user-box">
-                <input type="password" name="" required  onChange={ (e) => setPassword(e.target.value)} />
-                <label>Password</label>
+              <input
+                type="password"
+                name=""
+                required
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <label>Password</label>
             </div>
             <div className="button-wrapper" href="#">
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                <button type="submit">
-                  Submit
-                </button>
-               
-               </div> 
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <button type="submit">Submit</button>
+            </div>
 
-            <h6 className="mt-4" >Don't have an account?
-             <Link to= {`/signup?redirect=${redirect }`}>
-                Sign up
-            </Link> </h6>   
-        </form>
-    </div>
-    </div>
+            <h5>or</h5>
+
+            <GoogleLogin
+              clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+              buttonText="Sign in with Google"
+              onSuccess={handleLogin}
+              onFailure={handleFailure}
+              cookiePolicy={"single_host_origin"}
+            />
+
+            <h6 className="mt-4">
+              Don't have an account?
+              <Link to={`/signup?redirect=${redirect}`}>Sign up</Link>{" "}
+            </h6>
+          </form>
+        </div>
+      </div>
     </>
   );
 }
