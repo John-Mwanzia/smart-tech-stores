a7b739980969aa861949a9ee97d184ac2700c522
John mwanzia <johnmwanzia277@gmail.com>
2023-05-10 15:28:15 +0300

    added some comments in my Slider code

--- frontend/src/components/Slider.jsx
+++ frontend/src/components/Slider.jsx
@@ -1,27 +1,35 @@
 import React, { useEffect, useState } from "react";
 
 export default function Slider() {
+  // Initialize the current image index state variable
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
+  // Array of image paths to display in the slider
   const images = [
     "images/image1.png",
     "images/image2.png",
     "images/image3.png",
   ];
 
-const nextSlide = () => {
-  const newIndex = currentImageIndex === images.length - 1 ? 0 : currentImageIndex + 1;
-  setCurrentImageIndex(newIndex);
-};
+  // Function to switch to the next slide
+  const nextSlide = () => {
+    // Determine the index of the next slide based on the current image index
+    const newIndex =
+      currentImageIndex === images.length - 1 ? 0 : currentImageIndex + 1;
 
+    // Set the state variable to the new index to update the image displayed in the slider
+    setCurrentImageIndex(newIndex);
+  };
 
+  // Set up an interval to switch to the next slide every 3 seconds
   useEffect(() => {
-    const intervalId = setInterval(nextSlide, 3000);
-    return () => clearInterval(intervalId);
-  }, [nextSlide]);
+    const intervalId = setInterval(nextSlide, 3000); // Set up the interval with the nextSlide function and a 3 second delay
+    return () => clearInterval(intervalId); // Clean up the interval when the component unmounts
+  }, [nextSlide]); // Include the nextSlide function as a dependency to re-run the effect if it changes
 
   return (
     <>
+      {/* Display the current image in a container */}
       <div className="slider-container ">
         <img src={images[currentImageIndex]} alt="slider" />
       </div>
