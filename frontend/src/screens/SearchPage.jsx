3482cdbf7c392cc20cf22fcb9882a34586d7c3e9
John mwanzia <johnmwanzia277@gmail.com>
2023-05-11 17:35:46 +0300

    edited the search page

--- frontend/src/screens/SearchPage.jsx
+++ frontend/src/screens/SearchPage.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
 
 export default function SearchPage() {
+    const navigate = useNavigate();
+    const {search} = useLocation();
+    const sp = new URLSearchParams(search);
+    const query = sp.get('query' || 'all');
+    const category = sp.get('category' || 'all');
   return (
     <div>
       
