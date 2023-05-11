22f5c5b6fc02b80cc6491892b32a272c9bb339a7
John mwanzia <johnmwanzia277@gmail.com>
2023-05-12 00:14:34 +0300

    removed react bootstrap

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Button, Card, Col, Container, Row } from "react-bootstrap";
 import { Link, Route, Routes } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
 import CartScreen from "./screens/CartScreen";
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { HelmetProvider } from 'react-helmet-async';
 import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 import App from './App';
 import StoreProvider from './store';
 
