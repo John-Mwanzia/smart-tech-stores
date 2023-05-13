e488b740b01fb8e9252a003dd329fb2f3d478c48
John mwanzia <johnmwanzia277@gmail.com>
2023-05-13 09:45:53 +0300

    comments on product router

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -13,32 +13,39 @@ productRouter.get(
   "/search",
   expressAsyncHandler(async (req, res) => {
      /* query is an object that has category and query properties */
+      // Retrieve category and search query from the request's query string
     const category = req.query.category || "";
     const searchQuery = req.query.query || "";
 
     console.log('searchQuery:', searchQuery);
     console.log('category:', category);
+
+     // Define a query filter based on the search query
     const queryFilter =
       searchQuery && searchQuery !== "all"
         ? { Comp_Name: { $regex: searchQuery, $options: "i" } }
         : {};
 
         console.log('queryFilter:', queryFilter);
+      // Define a category filter based on the category
 
     const categoryFilter = category && category !== "all" ? { category } : {};
      console.log('categoryFilter:', categoryFilter);
     
 
+      // Find products that match the query and category filters
     const products = await Product.find({
       ...categoryFilter,
       ...queryFilter,
     });
 
  console.log('products:', products);
+    // Count the number of products that match the query and category filters
     const countProducts = await Product.countDocuments({
       ...categoryFilter,
       ...queryFilter,
     });
+      // Send the products and their count as a response
     res.send({
       products,
       countProducts,
@@ -50,8 +57,10 @@ productRouter.get(
 productRouter.get(
   "/categories",
   expressAsyncHandler(async (req, res) => {
+     // Retrieve all distinct categories from the products collection
     const categories = await Product.find().distinct("category");
     console.log('categories:', categories);
+      // Send the categories as a response
     res.send(categories);
   })
 );
