ba369589f01ef857e9952e14cece703877e2bf45
John mwanzia <johnmwanzia277@gmail.com>
2023-05-23 12:53:33 +0300

    returned deleted postcss file

---
+++ frontend/postcss.config.cjs
@@ -0,0 +1,33 @@
+ module.exports = {
+   plugins: {
+    tailwindcss: {
+       content: [
+         "./index.html",
+          "./src/**/*.{js,ts,jsx,tsx}",
+       ],
+        theme: {
+          extend: {
+            colors: {
+              customBlue: '#AEE2FF',
+              btnOrange: 'rgb(255, 166, 0)',
+              customGreen: 'rgba(62, 151, 108, 0.25)',
+              customGray: '#FAFAFB',
+              successGreen: '#4BB543',
+             
+             },
+             screens: {
+              sm: '640px',
+              md: '768px',
+              lg: '1024px',
+              xl: '1280px',
+            },
+            fontFamily: {
+              'sans': ['Outfit', 'sans-serif'],
+            },
+          },
+        },
+        plugins: [],
+      },
+      autoprefixer: {},
+    },
+   }
