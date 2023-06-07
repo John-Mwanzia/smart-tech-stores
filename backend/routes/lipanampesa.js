96740f1621cd311f6ae7410659c59a3a4bd109de
John mwanzia <johnmwanzia277@gmail.com>
2023-06-07 10:26:46 +0300

    edit lipa na mpesa route

--- backend/routes/lipanampesa.js
+++ backend/routes/lipanampesa.js
@@ -9,8 +9,13 @@ import {
 //middleware  for the authorization of all Safaricom requests.
 import {accessToken} from "../middlewares/middlewares.generateAccessToken.js";
 
+//stkPush route will initiate stk push popup on the users phone.
 router.route('/stkPush').post(accessToken,initiateSTKPush)
+
+//route Safaricom sends the results of the stk push.
 router.route('/stkPushCallback/:Order_ID').post(stkPushCallback)
+
+//route will use the CheckoutRequestID to confirm payment details. The CheckoutRequestID comes from successfully executing the stk push.
 router.route('/confirmPayment/:CheckoutRequestID').post(accessToken,confirmPayment)
 
 export default router
