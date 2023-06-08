9eeb7b8d310ef5c72b4197cddcf90af0c84c2163
John mwanzia <johnmwanzia277@gmail.com>
2023-06-08 11:14:49 +0300

    add helmet to shipping screen

--- frontend/src/screens/ShippingPage.jsx
+++ frontend/src/screens/ShippingPage.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Helmet } from 'react-helmet-async'
 
 export default function ShippingPage() {
   return (
     <div>
+      <Helmet>
+        <title>Shipping </title>
+      </Helmet>
       
     </div>
   )
