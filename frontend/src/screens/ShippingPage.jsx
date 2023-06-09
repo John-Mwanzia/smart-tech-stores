d2c730cc4fdcc4aed4d8f4702cc982e5440bcc80
John mwanzia <johnmwanzia277@gmail.com>
2023-06-09 20:27:52 +0300

    finish up shipping page

--- frontend/src/screens/ShippingPage.jsx
+++ frontend/src/screens/ShippingPage.jsx
@@ -9,13 +9,13 @@ export default function ShippingPage() {
       </Helmet>
 
       <div className="flex justify-center">
-        <div className="w-[600px]">
-          <h1 className="text-3xl font-sans font-semibold mt-8 mb-8">
-            Shipping information
-          </h1>
+        <div className="w-[800px]">
           <form className="w-full">
-            <div className="flex gap-x-8 justify-between">
-              <div>
+            <div className="flex gap-x-16 justify-between flex-wrap">
+              <div className="flex-1">
+                <h1 className="text-3xl font-sans font-semibold mt-8 mb-8">
+                  Shipping information
+                </h1>
                 <div className="flex flex-col ">
                   <label>Full Name</label>
                   <input
@@ -53,21 +53,35 @@ export default function ShippingPage() {
                 </div>
               </div>
               <div>
-                <div>
+                <h1 className="text-3xl font-sans font-semibold mt-8 mb-8  ">
+                  Payment method
+                </h1>
+                <div className="flex gap-x-4">
                   <input type="radio" name="payment" value="paypal" />
                   <label>
                     <img
                       src="https://logos-world.net/wp-content/uploads/2020/07/PayPal-Logo.png"
                       alt="paypal"
+                      className="h-12"
                     />
                   </label>
                 </div>
-                <div>
-                  <input type="radio" name="payment" value="stripe" />
-                  <label>Stripe</label>
+                <div className="flex gap-x-4">
+                  <input type="radio" name="payment" value="mpesa" />
+                  <label>
+                    <img
+                      src="../images/shipping/mpesa.svg"
+                      alt="mpesa"
+                      className="h-12"
+                    />
+                  </label>
                 </div>
+                <div className=" grid mt-8">
+                <button className=" bg-indigo-700 text-white px-4 py-2 rounded-xl">Continue</button>
+            </div>
               </div>
             </div>
+           
           </form>
         </div>
       </div>
