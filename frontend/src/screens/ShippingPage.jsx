2bd39f953a32c104cebae3246aec9595ee2dd7f2
John mwanzia <johnmwanzia277@gmail.com>
2023-06-09 17:55:13 +0300

    clean up shipping page

--- frontend/src/screens/ShippingPage.jsx
+++ frontend/src/screens/ShippingPage.jsx
@@ -10,30 +10,30 @@ export default function ShippingPage() {
       <h1 className="">Shipping information</h1>
 
       <div className="flex justify-center">
-         <div className="w-[600px]">
-         <form className="w-full">
-          <div className="flex flex-col ">
-            <label>Full Name</label>
-            <input type="text" className="border-2 rounded-xl" />
-          </div>
-          <div className="flex flex-col">
-            <label>Phone Number</label>
-            <input type="text" className="border-2" />
-          </div>
-          <div className="flex flex-col">
-            <label>Address</label>
-            <input type="text" className="border-2" />
-          </div>
-          <div className="flex flex-col">
-            <label>City</label>
-            <input type="text" className="border-2" />
-          </div>
-          <div className="flex flex-col">
-            <label>Postal code</label>
-            <input type="text" className="border-2" />
-          </div>
-        </form>
-         </div>
+        <div className="w-[600px]">
+          <form className="w-full">
+            <div className="flex flex-col ">
+              <label>Full Name</label>
+              <input type="text" className="border-2 rounded-xl" />
+            </div>
+            <div className="flex flex-col">
+              <label>Phone Number</label>
+              <input type="text" className="border-2 rounded-xl" />
+            </div>
+            <div className="flex flex-col">
+              <label>Address</label>
+              <input type="text" className="border-2 rounded-xl" />
+            </div>
+            <div className="flex flex-col">
+              <label>City</label>
+              <input type="text" className="border-2 rounded-xl" />
+            </div>
+            <div className="flex flex-col">
+              <label>Postal code</label>
+              <input type="text" className="border-2 rounded-xl" />
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
