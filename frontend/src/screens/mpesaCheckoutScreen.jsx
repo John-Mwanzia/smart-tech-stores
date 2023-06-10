643fab7cfd9fb04e31ddf9bd0eb89ccf414f871c
John mwanzia <johnmwanzia277@gmail.com>
2023-06-10 12:04:15 +0300

    create mpesa and paypal checkot page

---
+++ frontend/src/screens/mpesaCheckoutScreen.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function mpesaCheckoutScreen() {
+  return (
+    <div>
+      <h1>Mpesa Checkout</h1>
+    </div>
+  );
+}
---
+++ frontend/src/screens/paypalCheckoutScreen.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function paypalCheckoutScreen() {
+  return (
+    <div> 
+       <h1>
+        Paypal Checkout
+       </h1>
+    </div>
+  )
+}
