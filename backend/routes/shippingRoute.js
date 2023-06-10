2a65f76796b81e1944f97d7c7c7939ea43df3cde
John mwanzia <johnmwanzia277@gmail.com>
2023-06-10 10:46:56 +0300

    create shipping route

---
+++ backend/routes/shippingRoute.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import Shipping from '../models/ShippingModel.js';
+
+const shippingRoute = express.Router();
+
+shippingRoute.post('/', expressAsyncHandler(async (req, res) => {
+    const { paymentMethod, fullname, phoneNumber, address, city, postalCode } = req.body;
+    const newShipping = new Shipping({
+        paymentMethod: paymentMethod,
+        fullname: fullname,
+        phoneNumber: phoneNumber,
+        address: address,
+        city: city,
+        postalCode: postalCode,
+    });
+
+    const createdShipping = await newShipping.save();
+    res.status(201).send({ message: 'Shipping Created', shipping: createdShipping });
+}));
+
+export default shippingRoute;
