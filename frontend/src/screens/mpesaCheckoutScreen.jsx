9ea5b7510c56e1558ff260b93a78a45016741d63
John mwanzia <johnmwanzia277@gmail.com>
2023-06-11 13:49:02 +0300

    create gradient for mpesa checkout background

--- frontend/src/screens/mpesaCheckoutScreen.jsx
+++ frontend/src/screens/mpesaCheckoutScreen.jsx
@@ -2,8 +2,10 @@ import React from "react";
 
 export default function MpesaCheckoutScreen() {
   return (
-    <div>
-      <h1>Mpesa Checkout</h1>
-    </div>
+    <>
+      <div className="bg-gradient-to-br from-[#dbc596] to-[#70a24f] h-screen">
+        <h1>Mpesa Checkout</h1>
+      </div>
+    </>
   );
 }
