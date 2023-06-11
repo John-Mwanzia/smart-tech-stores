16ff15d74f3a248a3e6c392a850b91224334738c
John mwanzia <johnmwanzia277@gmail.com>
2023-06-11 14:02:11 +0300

    centered all divs

--- frontend/src/screens/mpesaCheckoutScreen.jsx
+++ frontend/src/screens/mpesaCheckoutScreen.jsx
@@ -3,11 +3,11 @@ import React from "react";
 export default function MpesaCheckoutScreen() {
   return (
     <>
-      <div className="bg-gradient-to-br from-[#dbc596] to-[#70a24f] h-screen">
-          <div>
-              
+      <div className=" flex items-center justify-center  bg-gradient-to-br from-[#dbc596] to-[#70a24f] h-screen">
+          <div className="h-[85%]">
+              <img src="/images/mpesaCheckout/lambs.svg" alt="lambs" className="h-[95%]"   />
           </div>
-          <div>
+          <div className="bg-white">
               <h1 className="text-[56px] font-semibold">Checkout</h1>
           </div>
       </div>
