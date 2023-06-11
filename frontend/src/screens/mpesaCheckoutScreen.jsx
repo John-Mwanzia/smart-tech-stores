14c73690551fde43b130b8e6da00b577042cff75
John mwanzia <johnmwanzia277@gmail.com>
2023-06-11 13:54:51 +0300

    add lambs svg

--- frontend/src/screens/mpesaCheckoutScreen.jsx
+++ frontend/src/screens/mpesaCheckoutScreen.jsx
@@ -4,7 +4,12 @@ export default function MpesaCheckoutScreen() {
   return (
     <>
       <div className="bg-gradient-to-br from-[#dbc596] to-[#70a24f] h-screen">
-        <h1>Mpesa Checkout</h1>
+          <div>
+              
+          </div>
+          <div>
+              <h1 className="text-[56px] font-semibold">Checkout</h1>
+          </div>
       </div>
     </>
   );
