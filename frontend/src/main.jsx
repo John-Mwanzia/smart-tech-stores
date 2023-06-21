2e9d853a082be2630e79c27c0fc925ba91b1f6c0
John mwanzia <johnmwanzia277@gmail.com>
2023-06-21 20:55:11 +0300

    remove google auth provider in the root

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 import StoreProvider from './store';
-import { GoogleOAuthProvider } from '@react-oauth/google';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
