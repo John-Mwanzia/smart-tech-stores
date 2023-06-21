1fbd7e62e93a1cd6d85a0e66683d237e617b9ea3
John mwanzia <johnmwanzia277@gmail.com>
2023-06-21 20:53:56 +0300

    edit header section

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,5 +1,4 @@
 import React, { useContext, useState } from "react";
-// import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 import { Link } from "react-router-dom";
 import { Store } from "../store";
