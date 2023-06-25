cb1b60622bbc6461baece6a628e6902d5ea7bd82
John mwanzia <johnmwanzia277@gmail.com>
2023-06-25 11:40:08 +0300

    account for xl screens in hero section

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -23,7 +23,7 @@ export default function HeroSection() {
           </select>
         </div>
 
-        <div className="flex justify-center gap-24 md:gap-8 lg:gap-24 flex-col md:flex-row">
+        <div className="flex justify-center gap-24 md:gap-8 lg:gap-24 flex-col md:flex-row xl:px-12">
           <div>
             <div>
               <h1 className="font-sans font-bold text-3xl px-4  lg:px-0 lg:text-5xl ">
