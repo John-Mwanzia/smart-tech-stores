aceafecc5e80c47ee086af5d0ee72a8e9c801d74
John mwanzia <johnmwanzia277@gmail.com>
2023-07-06 22:04:03 +0300

    edit tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -22,12 +22,6 @@ theme: {
         '2xl': {'min': '1280px'},
       },
   
-      // screens: {
-      //   sm: '640px',
-      //   md: '768px',
-      //   lg: '1024px',
-      //   xl: '1280px',
-      // },
       fontFamily: {
         'sans': ['Outfit', 'sans-serif'],
       },
