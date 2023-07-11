581a1effb8d03c1449138269300e0dfe4ef611a3
John mwanzia <johnmwanzia277@gmail.com>
2023-07-11 10:10:03 +0300

    rename utils file to auth in the modules

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,7 +2,7 @@ import express from "express"
 import User from "../models/userModel.js";
 import expressAsyncHandler from "express-async-handler"
 import bcrypt from "bcryptjs"
-import generateToken from "../modules/utils.js"
+import generateToken from "../modules/auth.js"
 
 const userRouter = express.Router();
 
