f453d8413dd2bcd27fbfeb5daf4aaa2e03b948a1
John mwanzia <johnmwanzia277@gmail.com>
2023-08-11 21:03:30 +0300

    clean up code

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
-import express  from "express";
-import cors from "cors"
+import express from "express";
+import cors from "cors";
 import mongoose from "mongoose";
-import * as dotenv from 'dotenv'
+import * as dotenv from "dotenv";
 import seedRouter from "./routes/seedRoutes.js";
 import productRouter from "./routes/productRoutes.js";
 import featuredProductsRouter from "./routes/featuredProductsRoutes.js";
@@ -11,35 +11,38 @@ import shippingRoute from "./routes/shippingRoute.js";
 import { protect } from "./modules/auth.js";
 import morgan from "morgan";
 
-
-dotenv.config()
+dotenv.config();
 const app = express();
-const port = process.env.PORT
+const port = process.env.PORT;
 
 //convert form data in the post request to json object
 app.use(express.json());
-app.use(express.urlencoded({extended: true}))
-app.use(cors())
-app.use(morgan('dev'));// logs every request to the console
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(morgan("dev")); // logs every request to the console
 
-mongoose.connect(process.env.MONGODB_URL, {useNewUrlParser: true, useUnifiedTopology: true}).then(()=>{
-  console.log("connected to db");
-}).catch(err=> console.log(err));
-app.use("/api/seed", seedRouter)
-app.use("/api/products", productRouter)
-app.use("/api/featuredProducts", featuredProductsRouter)
-app.use("/api/users", userRouter)
-app.use('/api/lipaNaMpesa',lipaNaMpesaRoute)
-app.use('/api/shipping', protect,shippingRoute)
+mongoose
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch((err) => console.log(err));
+app.use("/api/seed", seedRouter);
+app.use("/api/products", productRouter);
+app.use("/api/featuredProducts", featuredProductsRouter);
+app.use("/api/users", userRouter);
+app.use("/api/lipaNaMpesa", lipaNaMpesaRoute);
+app.use("/api/shipping", protect, shippingRoute);
 
 //error handling middleware
-app.use((err,req,res,next)=>{   
-    res.status(500).send({message: err.message})
-})
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 
-app.listen(port,  (error) =>{
-    if(!error)
-        console.log(`Server  Successfully started on port ${port}`)
-    else 
-        console.log("Error occurred, server can't start", error);
-    });
+app.listen(port, (error) => {
+  if (!error) console.log(`Server  Successfully started on port ${port}`);
+  else console.log("Error occurred, server can't start", error);
+});
