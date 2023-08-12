52a9e7825c5e906d871c5745c308258abf9ae661
John mwanzia <johnmwanzia277@gmail.com>
2023-08-12 23:58:26 +0300

    clean up code

--- backend/routes/lipanampesa.js
+++ backend/routes/lipanampesa.js
@@ -1,22 +1,23 @@
-import express from 'express'
-const lipaNaMpesaRoute = express.Router()
+import express from "express";
+const lipaNaMpesaRoute = express.Router();
 import {
-    initiateSTKPush,
-    stkPushCallback,
-    confirmPayment
-} from "../controllers/lipanampesa.js"
+  initiateSTKPush,
+  stkPushCallback,
+  confirmPayment,
+} from "../controllers/lipanampesa.js";
 
 //middleware  for the authorization of all Safaricom requests.
-import {accessToken} from "../middlewares/generateAccessToken.js"
+import { accessToken } from "../middlewares/generateAccessToken.js";
 
 //stkPush route will initiate stk push popup on the users phone.
-lipaNaMpesaRoute.route('/stkPush').post(accessToken,initiateSTKPush)
+lipaNaMpesaRoute.route("/stkPush").post(accessToken, initiateSTKPush);
 
 //route Safaricom sends the results of the stk push.
-lipaNaMpesaRoute.route('/stkPushCallback/:Order_ID').post(stkPushCallback)
+lipaNaMpesaRoute.route("/stkPushCallback/:Order_ID").post(stkPushCallback);
 
 //route will use the CheckoutRequestID to confirm payment details. The CheckoutRequestID comes from successfully executing the stk push.
-lipaNaMpesaRoute.route('/confirmPayment/:CheckoutRequestID').post(accessToken,confirmPayment)
+lipaNaMpesaRoute
+  .route("/confirmPayment/:CheckoutRequestID")
+  .post(accessToken, confirmPayment);
 
-
-export default lipaNaMpesaRoute
+export default lipaNaMpesaRoute;
