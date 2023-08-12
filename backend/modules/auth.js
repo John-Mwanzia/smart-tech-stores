53d23ea829a1b5f966091177faf11ad1092b35ad
John mwanzia <johnmwanzia277@gmail.com>
2023-08-12 23:57:09 +0300

    clean up code

--- backend/modules/auth.js
+++ backend/modules/auth.js
@@ -1,40 +1,38 @@
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
-export const generateToken = (user)=>{
-    return jwt.sign(
-        {
-        _id: user._id,
-        name: user.name,
-        email: user.email,
-        isAdmin: user.isAdmin
-
-
-    },
-     process.env.JWT_SECRET , 
+export const generateToken = (user) => {
+  return jwt.sign(
     {
-        expiresIn: "20d"
-    })
-}
-
-export const protect = (req,res,next)=>{
-    const bearer = req.headers.authorization;
-
-    if(!bearer || !bearer.startsWith("Bearer")){
-        res.status(401).json({message: "No token, authorization denied"})
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "20d",
     }
+  );
+};
 
-    const [,token] = bearer.split(" ");
+export const protect = (req, res, next) => {
+  const bearer = req.headers.authorization;
 
-    if(!token){
-        res.status(401).json({message: "No token, authorization denied"})
-    }
+  if (!bearer || !bearer.startsWith("Bearer")) {
+    res.status(401).json({ message: "No token, authorization denied" });
+  }
 
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded;
-        next();
-    }
-    catch (error) {
-        res.status(401).json({message: "Token is not valid"})
-    }
-}
+  const [, token] = bearer.split(" ");
+
+  if (!token) {
+    res.status(401).json({ message: "No token, authorization denied" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Token is not valid" });
+  }
+};
