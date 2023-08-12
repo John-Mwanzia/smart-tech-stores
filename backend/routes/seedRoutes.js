abcb0e656bc83de4ddbdad86fc62be82bec4674c
John mwanzia <johnmwanzia277@gmail.com>
2023-08-12 23:58:05 +0300

    clean up code

--- backend/routes/seedRoutes.js
+++ backend/routes/seedRoutes.js
@@ -1,4 +1,4 @@
-import express from "express"
+import express from "express";
 import Product from "../models/ProductsModel.js";
 import data from "../data.js";
 import FeaturedProducts from "../models/featuredProductsModel.js";
@@ -6,26 +6,24 @@ import User from "../models/userModel.js";
 
 const seedRouter = express.Router();
 
-seedRouter.get("/", async(req, res)=>{
-    //remove all products first in the product model to prevent duplicates
-      await Product.deleteMany({});
+seedRouter.get("/", async (req, res) => {
+  //remove all products first in the product model to prevent duplicates
+  await Product.deleteMany({});
 
-    const createdProducts =  await Product.insertMany(data.products)
-    //send data to the frontend
+  const createdProducts = await Product.insertMany(data.products);
+  //send data to the frontend
 
-// Featured products
-    await FeaturedProducts.deleteMany({});
+  // Featured products
+  await FeaturedProducts.deleteMany({});
 
-    const createdfeaturedProducts =  await FeaturedProducts.insertMany(data.featuredProducts)
+  const createdfeaturedProducts = await FeaturedProducts.insertMany(
+    data.featuredProducts
+  );
   //seed users
-    await User.deleteMany({});
+  await User.deleteMany({});
 
-    const createdUsers =  await User.insertMany(data.users)
-    res.send({createdProducts, createdfeaturedProducts, createdUsers})
-  
-
-
-
-})
+  const createdUsers = await User.insertMany(data.users);
+  res.send({ createdProducts, createdfeaturedProducts, createdUsers });
+});
 
 export default seedRouter;
