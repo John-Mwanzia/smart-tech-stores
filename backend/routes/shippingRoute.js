d891f7885e8b442172e6e4ee831a9399f358aee8
John mwanzia <johnmwanzia277@gmail.com>
2023-08-12 23:57:55 +0300

    clean up code

--- backend/routes/shippingRoute.js
+++ backend/routes/shippingRoute.js
@@ -1,22 +1,34 @@
-import express from 'express';
-import Shipping from '../models/ShippingModel.js';
-import expressAsyncHandler from 'express-async-handler';
+import express from "express";
+import Shipping from "../models/ShippingModel.js";
+import expressAsyncHandler from "express-async-handler";
 
 const shippingRoute = express.Router();
 
-shippingRoute.post('/', expressAsyncHandler(async (req, res) => {
-    const { paymentMethod, fullname, phoneNumber, address, city, postalCode } = req.body;
+shippingRoute.post(
+  "/",
+  expressAsyncHandler(async (req, res) => {
+    const {
+      paymentMethod,
+      fullname,
+      phoneNumber,
+      address,
+      city,
+      postalCode,
+    } = req.body;
     const newShipping = new Shipping({
-        paymentMethod: paymentMethod,
-        fullname: fullname,
-        phoneNumber: phoneNumber,
-        address: address,
-        city: city,
-        postalCode: postalCode,
+      paymentMethod: paymentMethod,
+      fullname: fullname,
+      phoneNumber: phoneNumber,
+      address: address,
+      city: city,
+      postalCode: postalCode,
     });
 
     const createdShipping = await newShipping.save();
-    res.status(201).send({ message: 'Shipping Created', shipping: createdShipping });
-}));
+    res
+      .status(201)
+      .send({ message: "Shipping Created", shipping: createdShipping });
+  })
+);
 
 export default shippingRoute;
