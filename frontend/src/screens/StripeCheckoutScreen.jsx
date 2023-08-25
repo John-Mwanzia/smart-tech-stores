b9a36bffc5cf3b7d80cadd885c1bddfa1ef6c8ac
John mwanzia <johnmwanzia277@gmail.com>
2023-08-25 13:48:20 +0300

    finish up stripe intergration

--- frontend/src/screens/StripeCheckoutScreen.jsx
+++ frontend/src/screens/StripeCheckoutScreen.jsx
@@ -1,19 +1,20 @@
-import React, { useContext } from 'react'
-import { Store } from '../store';
-import axios from 'axios';
-import { loadStripe } from '@stripe/stripe-js';
+import React, { useContext } from "react";
+import { Store } from "../store";
+import axios from "axios";
+import { loadStripe } from "@stripe/stripe-js";
 
-const StripeCheckoutScreen = async()=> {
-  const stripePromise = loadStripe(import.meta.env.VITE_APP_STRIPE_PUBLISHABLE_KEY)
-  const { state} = useContext(Store);
+const StripeCheckoutScreen = async () => {
+  const stripePromise = loadStripe(
+    import.meta.env.VITE_APP_STRIPE_PUBLISHABLE_KEY
+  );
+  const { state } = useContext(Store);
   const { cart } = state;
   const { cartItems } = cart;
-
 
   const lineItems = cartItems.map((item) => {
     return {
       price_data: {
-        currency: 'usd',
+        currency: "usd",
         product_data: {
           name: item.Gadget_Name || item.Comp_Name,
           images: [item.Img_Url],
@@ -21,12 +22,14 @@ const StripeCheckoutScreen = async()=> {
         unit_amount: item.price * 100, // Stripe expects the price in a currencies smallest unit
       },
       quantity: item.quantity,
-    }
-  })
-//https://smart-server.vercel.app/api/checkout
-  const {data} = await axios.post("http://localhost:3000/api/checkout", {
-    lineItems,
+    };
   });
+  const { data } = await axios.post(
+    "https://smart-server.vercel.app/api/checkout",
+    {
+      lineItems,
+    }
+  );
   const stripe = await stripePromise;
   const { error } = await stripe.redirectToCheckout({
     sessionId: data.id,
@@ -35,14 +38,11 @@ const StripeCheckoutScreen = async()=> {
     alert(error.message);
   }
 
-
   return (
-    <div> 
-       <h1>
-        Paypal Checkout
-       </h1>
+    <div>
+      <h1>Paypal Checkout</h1>
     </div>
-  )
-}
+  );
+};
 
-export default StripeCheckoutScreen
+export default StripeCheckoutScreen;
