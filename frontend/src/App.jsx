577f5f5839c8b5fcb2e7fecab2ac959c9b991ae7
John mwanzia <johnmwanzia277@gmail.com>
2023-08-26 15:36:39 +0300

    import cancelPage screen in the app.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ import Support from "./screens/Support";
 import QA from "./screens/QA";
 import StripeCheckoutScreen from "./screens/StripeCheckoutScreen";
 import SuccessScreen from "./screens/Success_Screen";
+import CancelPage from "./screens/CancelPage";
 
 function App() {
   return (
@@ -40,6 +41,7 @@ function App() {
             <Route path="/Support" element={<Support />} />
             <Route path="/QA" element={<QA />} />
             <Route path="/success" element={<SuccessScreen/>} />
+            <Route path="/cancel" element={<CancelPage/>} />
           </Routes>
         </main>
       </div>
