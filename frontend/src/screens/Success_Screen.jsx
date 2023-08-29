4a1ad947b0cff4a91b32b14864e67506edcbcf81
John mwanzia <johnmwanzia277@gmail.com>
2023-08-29 21:35:44 +0300

    center success page contents

--- frontend/src/screens/Success_Screen.jsx
+++ frontend/src/screens/Success_Screen.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function SuccessScreen() {
   return (
     <div>
-       <h1>Success</h1>
+       <h1 className='text-center text-4xl'>Success</h1>
     </div>
   )
 }
