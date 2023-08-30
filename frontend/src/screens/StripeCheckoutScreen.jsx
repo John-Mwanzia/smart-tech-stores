7a51ec494a24a4923e3010095d7de951a8b2a815
John mwanzia <johnmwanzia277@gmail.com>
2023-08-30 17:43:57 +0300

    handle asynchronous operations within a React functional component,by typically use the useEffect

--- frontend/src/screens/StripeCheckoutScreen.jsx
+++ frontend/src/screens/StripeCheckoutScreen.jsx
@@ -1,46 +1,117 @@
-import React, { useContext } from "react";
+// import React, { useContext } from "react";
+// import { Store } from "../store";
+// import axios from "axios";
+// import { loadStripe } from "@stripe/stripe-js";
+
+// const StripeCheckoutScreen = async () => {
+//   const stripePromise = loadStripe(
+//     import.meta.env.VITE_APP_STRIPE_PUBLISHABLE_KEY
+//   );
+//   const { state } = useContext(Store);
+//   const { cart } = state;
+//   const { cartItems } = cart;
+//   console.log(cartItems);
+
+//   const lineItems = cartItems.map((item) => {
+//     return {
+//       price_data: {
+//         currency: "kes",
+//         product_data: {
+//           name: item.Gadget_Name || item.Comp_Name,
+//           images: [item.Img_Url],
+//         },
+//         unit_amount: item.price * 100, // Stripe expects the price in a currencies smallest unit
+//       },
+//       quantity: item.quantity,
+//     };
+//   });
+//   const { data } = await axios.post(
+//     "https://smart-server.vercel.app/api/checkout",
+//     {
+//       lineItems,
+//     }
+//   );
+//   const stripe = await stripePromise;
+//   const { error } = await stripe.redirectToCheckout({
+//     sessionId: data.id,
+//   });
+//   if (error) {
+//     alert(error.message);
+//   }
+
+//   return (
+//     <div>
+//       <h1>Paypal Checkout</h1>
+//     </div>
+//   );
+// };
+
+// export default StripeCheckoutScreen;
+
+
+import React, { useContext, useEffect, useState } from "react";
 import { Store } from "../store";
 import axios from "axios";
 import { loadStripe } from "@stripe/stripe-js";
 
-const StripeCheckoutScreen = async () => {
+const StripeCheckoutScreen = () => {
   const stripePromise = loadStripe(
     import.meta.env.VITE_APP_STRIPE_PUBLISHABLE_KEY
   );
   const { state } = useContext(Store);
   const { cart } = state;
   const { cartItems } = cart;
+  const [loading, setLoading] = useState(true);
 
-  const lineItems = cartItems.map((item) => {
-    return {
-      price_data: {
-        currency: "kes",
-        product_data: {
-          name: item.Gadget_Name || item.Comp_Name,
-          images: [item.Img_Url],
-        },
-        unit_amount: item.price * 100, // Stripe expects the price in a currencies smallest unit
-      },
-      quantity: item.quantity,
+  useEffect(() => {
+    const fetchData = async () => {
+      const lineItems = cartItems.map((item) => {
+        return {
+          price_data: {
+            currency: "kes",
+            product_data: {
+              name: item.Gadget_Name || item.Comp_Name,
+              images: [item.Img_Url],
+            },
+            unit_amount: item.price * 100,
+          },
+          quantity: item.quantity,
+        };
+      });
+
+      try {
+        const { data } = await axios.post(
+          "https://smart-server.vercel.app/api/checkout",
+          {
+            lineItems,
+          }
+        );
+        
+        const stripe = await stripePromise;
+        const { error } = await stripe.redirectToCheckout({
+          sessionId: data.id,
+        });
+
+        if (error) {
+          alert(error.message);
+        }
+      } catch (error) {
+        console.error("Error:", error);
+      }
+
+      setLoading(false);
     };
-  });
-  const { data } = await axios.post(
-    "https://smart-server.vercel.app/api/checkout",
-    {
-      lineItems,
-    }
-  );
-  const stripe = await stripePromise;
-  const { error } = await stripe.redirectToCheckout({
-    sessionId: data.id,
-  });
-  if (error) {
-    alert(error.message);
-  }
+
+    fetchData();
+  }, []);
 
   return (
     <div>
-      <h1>Paypal Checkout</h1>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <h1>Paypal Checkout</h1>
+      )}
     </div>
   );
 };
