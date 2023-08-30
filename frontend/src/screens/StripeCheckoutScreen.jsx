3a0ed837c37f0ff2a4f96010d37e57209b036e4a
John mwanzia <johnmwanzia277@gmail.com>
2023-08-30 17:59:03 +0300

    remove unused code in stripe component

--- frontend/src/screens/StripeCheckoutScreen.jsx
+++ frontend/src/screens/StripeCheckoutScreen.jsx
@@ -1,54 +1,3 @@
-// import React, { useContext } from "react";
-// import { Store } from "../store";
-// import axios from "axios";
-// import { loadStripe } from "@stripe/stripe-js";
-
-// const StripeCheckoutScreen = async () => {
-//   const stripePromise = loadStripe(
-//     import.meta.env.VITE_APP_STRIPE_PUBLISHABLE_KEY
-//   );
-//   const { state } = useContext(Store);
-//   const { cart } = state;
-//   const { cartItems } = cart;
-//   console.log(cartItems);
-
-//   const lineItems = cartItems.map((item) => {
-//     return {
-//       price_data: {
-//         currency: "kes",
-//         product_data: {
-//           name: item.Gadget_Name || item.Comp_Name,
-//           images: [item.Img_Url],
-//         },
-//         unit_amount: item.price * 100, // Stripe expects the price in a currencies smallest unit
-//       },
-//       quantity: item.quantity,
-//     };
-//   });
-//   const { data } = await axios.post(
-//     "https://smart-server.vercel.app/api/checkout",
-//     {
-//       lineItems,
-//     }
-//   );
-//   const stripe = await stripePromise;
-//   const { error } = await stripe.redirectToCheckout({
-//     sessionId: data.id,
-//   });
-//   if (error) {
-//     alert(error.message);
-//   }
-
-//   return (
-//     <div>
-//       <h1>Paypal Checkout</h1>
-//     </div>
-//   );
-// };
-
-// export default StripeCheckoutScreen;
-
-
 import React, { useContext, useEffect, useState } from "react";
 import { Store } from "../store";
 import axios from "axios";
@@ -86,7 +35,7 @@ const StripeCheckoutScreen = () => {
             lineItems,
           }
         );
-        
+
         const stripe = await stripePromise;
         const { error } = await stripe.redirectToCheckout({
           sessionId: data.id,
@@ -105,15 +54,7 @@ const StripeCheckoutScreen = () => {
     fetchData();
   }, []);
 
-  return (
-    <div>
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <h1>Paypal Checkout</h1>
-      )}
-    </div>
-  );
+  return <div>{loading ? <h1>Loading...</h1> : <h1>Paypal Checkout</h1>}</div>;
 };
 
 export default StripeCheckoutScreen;
