fba11ac1fcf3d83c1e05d7dc72beea4e1579c8b9
John mwanzia <johnmwanzia277@gmail.com>
2023-09-03 15:28:36 +0300

    resize admin title

--- frontend/src/screens/AdminDashboard.jsx
+++ frontend/src/screens/AdminDashboard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function AdminDashboard() {
   return (
     <>
-      <h1 className="text-center">Admin Dashboard</h1>
+      <h1 className="text-center text-4xl">Admin Dashboard</h1>
     </>
   );
 }
