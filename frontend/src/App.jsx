224b019abf4078ceea6a51154c6c56c7eb853328
John mwanzia <johnmwanzia277@gmail.com>
2023-09-05 14:52:43 +0300

    remove div wrappper from the routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,35 +20,33 @@ import AdminDashboard from "./screens/AdminDashboard";
 function App() {
   return (
     <BrowserRouter>
-      <div>
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
 
-        <main>
-          <Routes>
-            <Route path="/products/slug/:slug" element={<ProductPage />} />
-            <Route
-              path="/featuredProducts/slug/:slug"
-              element={<FeaturedProductPage />}
-            />
-            <Route path="/cart" element={<CartScreen />} />
-            <Route path="/search" element={<SearchPage />} />
-            <Route path="/signin" element={<SignInPage />} />
-            <Route path="/signup" element={<SignUpPage />} />
-            <Route path="/shipping" element={<ShippingPage />} />
-            <Route path="/mpesaCheckout" element={<MpesaCheckoutScreen />} />
-            <Route path="/StripeCheckout" element={<StripeCheckoutScreen />} />
-            <Route path="/Support" element={<Support />} />
-            <Route path="/QA" element={<QA />} />
-            <Route path="/success" element={<SuccessScreen />} />
-            <Route path="/cancel" element={<CancelPage />} />
-          </Routes>
-        </main>
+      <main>
         <Routes>
-          <Route path="/admin" element={<AdminDashboard />} />
+          <Route path="/products/slug/:slug" element={<ProductPage />} />
+          <Route
+            path="/featuredProducts/slug/:slug"
+            element={<FeaturedProductPage />}
+          />
+          <Route path="/cart" element={<CartScreen />} />
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/signin" element={<SignInPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/shipping" element={<ShippingPage />} />
+          <Route path="/mpesaCheckout" element={<MpesaCheckoutScreen />} />
+          <Route path="/StripeCheckout" element={<StripeCheckoutScreen />} />
+          <Route path="/Support" element={<Support />} />
+          <Route path="/QA" element={<QA />} />
+          <Route path="/success" element={<SuccessScreen />} />
+          <Route path="/cancel" element={<CancelPage />} />
         </Routes>
-      </div>
+      </main>
+      <Routes>
+        <Route path="/admin" element={<AdminDashboard />} />
+      </Routes>
     </BrowserRouter>
   );
 }
