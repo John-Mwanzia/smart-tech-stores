48bbe53af84a026069e740e814ef44ab63da5a8a
John mwanzia <johnmwanzia277@gmail.com>
2023-09-13 16:02:01 +0300

    edit admin dashboard

--- frontend/src/screens/AdminDashboard.jsx
+++ frontend/src/screens/AdminDashboard.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 export default function AdminDashboard() {
   return (
     <>
-      <h1 className="text-center text-4xl">Admin Dashboard</h1>
+      
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
+            <TooltipComponent content="Settings" position="Top">
+              <button
+                type="button"
+                className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
+              >
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+        </div>
+     
     </>
   );
 }
