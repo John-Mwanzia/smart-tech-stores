853d4c5b090f38fec4d4a1b307179dd2d4eda9af
John mwanzia <johnmwanzia277@gmail.com>
2023-09-15 14:20:46 +0300

    add App.css file

--- frontend/src/screens/AdminDashboard.jsx
+++ frontend/src/screens/AdminDashboard.jsx
@@ -1,26 +1,33 @@
 import React from "react";
-import { FiSettings } from 'react-icons/fi';
-import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import { FiSettings } from "react-icons/fi";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
 export default function AdminDashboard() {
+  const activeMenu = true;
   return (
     <>
-      
-        <div className="flex relative dark:bg-main-dark-bg">
-        <h1>Hello</h1>
-          <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
-            <TooltipComponent content="Settings" position="Top">
-              <button
-                type="button"
-                className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
-                style={{background:"blue", borderRadius: '50%'}}
-              >
-                <FiSettings />
-              </button>
-            </TooltipComponent>
-          </div>
+      <div className="flex relative dark:bg-main-dark-bg">
+        <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
+          <TooltipComponent content="Settings" position="Top">
+            <button
+              type="button"
+              className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
+              style={{ background: "blue", borderRadius: "50%" }}
+            >
+              <FiSettings />
+            </button>
+          </TooltipComponent>
         </div>
-     
+        {activeMenu ? (
+          <div className="w-72 bg-white fixed sidebar dark:bg-secondary-dark-bg">
+            SideBar
+          </div>
+        ): (
+          <div>
+            SideBar w-0
+          </div>
+        )}
+      </div>
     </>
   );
 }
