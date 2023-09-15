ccedf0c6345caad4dc7898ecb35403ae98f182a1
John mwanzia <johnmwanzia277@gmail.com>
2023-09-15 14:36:12 +0300

    create components for admin dashboard

--- frontend/src/screens/AdminDashboard.jsx
+++ frontend/src/screens/AdminDashboard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { FiSettings } from "react-icons/fi";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import { Route, Routes } from "react-router-dom";
 
 export default function AdminDashboard() {
   const activeMenu = true;
@@ -22,11 +23,51 @@ export default function AdminDashboard() {
           <div className="w-72 bg-white fixed sidebar dark:bg-secondary-dark-bg">
             SideBar
           </div>
-        ): (
-          <div>
-            SideBar w-0
-          </div>
+        ) : (
+          <div className="w-0 dark:bg-secondary-dark-bg ">SideBar w-0</div>
         )}
+        <div
+          className={
+            activeMenu
+              ? "dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  "
+              : "bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 "
+          }
+        >
+          <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
+            <Navbar />
+          </div>
+          <div>
+            {themeSettings && <ThemeSettings />}
+
+            <Routes>
+              {/* dashboard  */}
+              <Route path="/" element={<Ecommerce />} />
+              <Route path="/ecommerce" element={<Ecommerce />} />
+
+              {/* pages  */}
+              <Route path="/orders" element={<Orders />} />
+              <Route path="/employees" element={<Employees />} />
+              <Route path="/customers" element={<Customers />} />
+
+              {/* apps  */}
+              <Route path="/kanban" element={<Kanban />} />
+              <Route path="/editor" element={<Editor />} />
+              <Route path="/calendar" element={<Calendar />} />
+              <Route path="/color-picker" element={<ColorPicker />} />
+
+              {/* charts  */}
+              <Route path="/line" element={<Line />} />
+              <Route path="/area" element={<Area />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/financial" element={<Financial />} />
+              <Route path="/color-mapping" element={<ColorMapping />} />
+              <Route path="/pyramid" element={<Pyramid />} />
+              <Route path="/stacked" element={<Stacked />} />
+            </Routes>
+          </div>
+          <Footer />
+        </div>
       </div>
     </>
   );
