4ba8510b5092d8a893e5b0fffd2be7e9fe05ef18
John mwanzia <johnmwanzia277@gmail.com>
2023-09-16 13:58:40 +0300

    wrap my app with context provider

--- frontend/src/context/AdminContext.jsx
+++ frontend/src/context/AdminContext.jsx
@@ -3,7 +3,6 @@ import React, { createContext, useContext, useState } from 'react';
 const StateContext = createContext();
 
 const initialState = {
-  chat: false,
   cart: false,
   userProfile: false,
   notification: false,
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,21 @@
-import React from 'react';
-import { HelmetProvider } from 'react-helmet-async';
-import ReactDOM from 'react-dom/client';
-import { registerLicense } from '@syncfusion/ej2-base';
+import React from "react";
+import { HelmetProvider } from "react-helmet-async";
+import ReactDOM from "react-dom/client";
+import { registerLicense } from "@syncfusion/ej2-base";
 
-import App from './App';
-import StoreProvider from './store';
-registerLicense( import.meta.env.VITE_SYNCFUSION_KEY)
+import App from "./App";
+import StoreProvider from "./store";
+import { ContextProvider } from "./context/AdminContext";
+registerLicense(import.meta.env.VITE_SYNCFUSION_KEY);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-  <HelmetProvider>
-   <StoreProvider>
-    <App />
-    </StoreProvider>
+    <HelmetProvider>
+      <StoreProvider>
+        <ContextProvider>
+          <App />
+        </ContextProvider>
+      </StoreProvider>
     </HelmetProvider>
   </React.StrictMode>
 );
