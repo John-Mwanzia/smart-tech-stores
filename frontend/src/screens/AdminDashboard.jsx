087a2667bdcbd729f3e3378dbaaa9171462b8d51
John mwanzia <johnmwanzia277@gmail.com>
2023-09-23 01:53:00 +0300

    fully functional darkmode setter

--- frontend/src/screens/AdminDashboard.jsx
+++ frontend/src/screens/AdminDashboard.jsx
@@ -40,7 +40,7 @@ export default function AdminDashboard() {
   } = useStateContext();
   return (
     <>
-    <div>
+    <div className={currentMode === 'Dark' ? 'dark' : ''}>
       <div className="flex relative dark:bg-main-dark-bg">
         <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
           <TooltipComponent content="Settings" position="Top">
