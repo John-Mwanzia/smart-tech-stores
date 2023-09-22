2433069a2dd8b30c27a8e70c57ad3c8fbf320a7b
John mwanzia <johnmwanzia277@gmail.com>
2023-09-23 01:46:27 +0300

    fix bgcolors defined in tailwind config not working

--- frontend/postcss.config.cjs
+++ frontend/postcss.config.cjs
@@ -24,6 +24,34 @@
             fontFamily: {
               'sans': ['Outfit', 'sans-serif'],
             },
+            
+            fontFamily: {
+              sans: ["Outfit", "sans-serif"],
+              roboto: ["Roboto", "sans-serif"],
+            },
+            borderWidth: {
+              1: "1px",
+            },
+            borderColor: {
+              color: "rgba(0, 0, 0, 0.1)",
+            },
+            height: {
+              80: "80px",
+            },
+            minHeight: {
+              590: "590px",
+            },
+            backgroundColor: {
+              "main-bg": "rgba(250, 251, 251, 1)",
+              "main-dark-bg": "rgba(32, 35, 42, 1)",
+              "secondary-dark-bg": "rgba(51, 55, 62, 1)",
+              "light-gray": "rgba(247, 247, 247, 1)",
+              "half-transparent": "rgba(0, 0, 0, 0.5)",
+            },
+            backgroundImage: {
+              "hero-pattern": "url('https://i.ibb.co/MkvLDfb/Rectangle-4389.png')",
+            },
+
           },
         },
         plugins: [],
