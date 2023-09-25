16f095e9d25110cc746084c93473561fc0de60a1
John mwanzia <johnmwanzia277@gmail.com>
2023-09-25 19:36:15 +0300

    clean up code

--- backend/routes/OrderRoute.js
+++ backend/routes/OrderRoute.js
@@ -1,13 +1,14 @@
-import express from 'express'
-import Order from '../models/orderModel'
+import express from "express";
+import Order from "../models/orderModel";
 
-const orderRouter = express.Router()
+const orderRouter = express.Router();
 
-orderRouter.post('/', expressAsyncHandler(async(req, res)=>{
+orderRouter.post(
+  "/",
+  expressAsyncHandler(async (req, res) => {
     console.log(req.body);
-    const newOrder = await Order({
-
-    })
-}))
+    const newOrder = await Order({});
+  })
+);
 
 export default orderRouter;
