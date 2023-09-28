f362f3c54476d92355bd042233ab8d1ae5cc8988
John mwanzia <johnmwanzia277@gmail.com>
2023-09-28 19:32:15 +0300

    code cleanup

--- frontend/src/screens/Admin/pages/Calendar.jsx
+++ frontend/src/screens/Admin/pages/Calendar.jsx
@@ -1,9 +1,21 @@
-import React, { useState } from 'react';
-import { ScheduleComponent, ViewsDirective, ViewDirective, Day, Week, WorkWeek, Month, Agenda, Inject, Resize, DragAndDrop } from '@syncfusion/ej2-react-schedule';
-import { DatePickerComponent } from '@syncfusion/ej2-react-calendars';
+import React, { useState } from "react";
+import {
+  ScheduleComponent,
+  ViewsDirective,
+  ViewDirective,
+  Day,
+  Week,
+  WorkWeek,
+  Month,
+  Agenda,
+  Inject,
+  Resize,
+  DragAndDrop,
+} from "@syncfusion/ej2-react-schedule";
+import { DatePickerComponent } from "@syncfusion/ej2-react-calendars";
 
-import { scheduleData } from '../../../data/dummy';
-import { Header } from '../../../components/AdminDashBoardComponents';
+import { scheduleData } from "../../../data/dummy";
+import { Header } from "../../../components/AdminDashBoardComponents";
 
 // eslint-disable-next-line react/destructuring-assignment
 const PropertyPane = (props) => <div className="mt-5">{props.children}</div>;
@@ -32,17 +44,19 @@ const Scheduler = () => {
         dragStart={onDragStart}
       >
         <ViewsDirective>
-          { ['Day', 'Week', 'WorkWeek', 'Month', 'Agenda'].map((item) => <ViewDirective key={item} option={item} />)}
+          {["Day", "Week", "WorkWeek", "Month", "Agenda"].map((item) => (
+            <ViewDirective key={item} option={item} />
+          ))}
         </ViewsDirective>
-        <Inject services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]} />
+        <Inject
+          services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]}
+        />
       </ScheduleComponent>
       <PropertyPane>
-        <table
-          style={{ width: '100%', background: 'white' }}
-        >
+        <table style={{ width: "100%", background: "white" }}>
           <tbody>
-            <tr style={{ height: '50px' }}>
-              <td style={{ width: '100%' }}>
+            <tr style={{ height: "50px" }}>
+              <td style={{ width: "100%" }}>
                 <DatePickerComponent
                   value={new Date(2021, 0, 10)}
                   showClearButton={false}
