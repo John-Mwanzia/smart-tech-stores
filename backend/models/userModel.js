8e4113f480865c9a7a48d2b3d2a54818547654bd
John mwanzia <johnmwanzia277@gmail.com>
2023-09-29 18:38:02 +0300

    code cleanup

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,16 +1,17 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
-    name: {type: "String", required: true},
-    email:{type: "String", required: true, unique: true},
-    password : {type: "String", required: true, unique: true},
-    isAdmin : {type: Boolean, default: false, required: true}
-},
-{
-    timestamps: true
-}
+const UserSchema = new Schema(
+  {
+    name: { type: "String", required: true },
+    email: { type: "String", required: true, unique: true },
+    password: { type: "String", required: true, unique: true },
+    isAdmin: { type: Boolean, default: false, required: true },
+  },
+  {
+    timestamps: true,
+  }
 );
-   
- const User = mongoose.model("User", UserSchema)
- export default User;
+
+const User = mongoose.model("User", UserSchema);
+export default User;
