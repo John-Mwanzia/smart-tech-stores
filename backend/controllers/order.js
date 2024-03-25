52f4422f72ce5055c99036c22327f2827249d4a7
John mwanzia <johnmwanzia277@gmail.com>
2024-03-25 21:42:01 +0300

    chore: decompose order router

---
+++ backend/controllers/order.js
@@ -0,0 +1,21 @@
+import expressAsyncHandler from "express-async-handler";
+import Order from "../models/orderModel";
+
+export const createOrder = expressAsyncHandler(async (req, res) => {
+    const newOrder = await Order({
+      orderItems: req.body.orderItems,
+      shippingAddress: req.body.shippingAddress,
+      paymentMethod: req.body.paymentMethod,
+      itemsPrice: req.body.itemsPrice,
+      shippingPrice: req.body.shippingPrice,
+      taxPrice: req.body.taxPrice,
+      totalPrice: req.body.totalPrice,
+      user: req.user,
+      isPaid: req.body.isPaid,
+      paidAt: req.body.paidAt,
+      isDelivered: req.body.isDelivered,
+      deliveredAt: req.body.deliveredAt,
+    });
+    const createdOrder = await newOrder.save();
+    res.status(201).json({ message: "New Order Created", order: createdOrder });
+  })
