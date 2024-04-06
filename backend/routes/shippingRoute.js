3b85258a3d0931d6e8b59a860131f804456e7683
John mwanzia <johnmwanzia277@gmail.com>
2024-04-06 20:59:01 +0300

    chore: create controller for shipping

--- backend/routes/shippingRoute.js
+++ backend/routes/shippingRoute.js
@@ -1,34 +1,8 @@
 import express from "express";
-import Shipping from "../models/ShippingModel.js";
-import expressAsyncHandler from "express-async-handler";
+import { createShipping } from "../controllers/shipping.js";
 
 const shippingRoute = express.Router();
 
-shippingRoute.post(
-  "/",
-  expressAsyncHandler(async (req, res) => {
-    const {
-      paymentMethod,
-      fullname,
-      phoneNumber,
-      address,
-      city,
-      postalCode,
-    } = req.body;
-    const newShipping = new Shipping({
-      paymentMethod: paymentMethod,
-      fullname: fullname,
-      phoneNumber: phoneNumber,
-      address: address,
-      city: city,
-      postalCode: postalCode,
-    });
-
-    const createdShipping = await newShipping.save();
-    res
-      .status(201)
-      .send({ message: "Shipping Created", shipping: createdShipping });
-  })
-);
+shippingRoute.post("/", createShipping);
 
 export default shippingRoute;
