a339354709fd5f75fd098ec38cff1b662880f054
John mwanzia <johnmwanzia277@gmail.com>
2024-04-13 20:10:18 +0300

    ci: seed products and users to database

--- backend/routes/seedRoutes.js
+++ backend/routes/seedRoutes.js
@@ -3,27 +3,10 @@ import Product from "../models/ProductsModel.js";
 import data from "../data.js";
 import FeaturedProducts from "../models/featuredProductsModel.js";
 import User from "../models/userModel.js";
+import { seed } from "../controllers/seed.js";
 
 const seedRouter = express.Router();
 
-seedRouter.get("/", async (req, res) => {
-  //remove all products first in the product model to prevent duplicates
-  await Product.deleteMany({});
-
-  const createdProducts = await Product.insertMany(data.products);
-  //send data to the frontend
-
-  // Featured products
-  await FeaturedProducts.deleteMany({});
-
-  const createdfeaturedProducts = await FeaturedProducts.insertMany(
-    data.featuredProducts
-  );
-  //seed users
-  await User.deleteMany({});
-
-  const createdUsers = await User.insertMany(data.users);
-  res.send({ createdProducts, createdfeaturedProducts, createdUsers });
-});
+seedRouter.get("/", seed);
 
 export default seedRouter;
